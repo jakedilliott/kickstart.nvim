b9f6a521702e81784696e1d706de8cb6f2f43527
= <=>
2025-06-05 18:43:07 -0600

    first commit on my neovim config

--- init.lua
+++ init.lua
@@ -918,6 +918,9 @@ require('lazy').setup({
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
+  -- Automatically generates paired parens, brackets, quotes and highlights matchining pairs.
+  'Raimondi/delimitMate',
+
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
